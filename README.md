6968b7f159a315211a400a3c4ba88292a850e741
egeatmaca <egeatmaca98@gmail.com>
2024-07-07 10:18:24 +0200

    custom config added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# arch-i3-postinstall
+# Arch I3 Postinstall
 A post-installation script for Arch with i3-WM
+
+## Screenhots
+#### A user-friendly tiling window manager
+![screen1](./screenshots/screen1.png)
+![screen2](./screenshots/screen2.png)
+#### Minimal resource usage (< 500 MB RAM)
+![screen3](./screenshots/screen3.png)
--- postinstall.sh
+++ postinstall.sh
@@ -1,15 +1,23 @@
-## Arch I3 Postinstall ###
+### Arch I3 Postinstall ###
 
 # Update mirrors
 sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist
 
-# Update and install packages
-sudo pacman -Syu --needed --noconfirm irefox ufw bluez bluez-utils blueman redshift git python-pip htop neofetch
+# Update and install pacman packages
+sudo pacman -Syu --needed --noconfirm alacritty rofi htop neofetch bluez bluez-utils blueman redshift firefox code git python-pip
+
+# Install and update yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+sudo rm -r yay
+yay
 
 # Enable installed services
 sudo systemctl enable --now bluetooth
 sudo ufw enable
 
-# Install i3 starterpack
-I3_STARTERPACK_URL="https://github.com/addy-dclxvi/i3-starterpack.git"
-git clone $I3_STARTERPACK_URL && cp -a i3-starterpack/. ~ && sudo rm -r i3-starterpack
+# Copy config files
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cp -a $SCRIPT_DIR/config/. ~/.config
