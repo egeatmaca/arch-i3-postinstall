cbd14e06c0d0e0dbb8e783445d9daf46ff1310e6
egeatmaca <egeatmaca98@gmail.com>
2024-07-13 17:43:02 +0200

    thunar file manager added

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,7 @@
 sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Update and install pacman packages
-sudo pacman -Syu --needed --noconfirm alacritty rofi feh picom htop neofetch bluez bluez-utils blueman redshift firefox ufw code neovim git python-pip docker docker-buildx docker-compose
+sudo pacman -Syu --needed --noconfirm alacritty rofi thunar feh picom htop neofetch bluez bluez-utils blueman redshift firefox ufw code neovim git python-pip docker docker-buildx docker-compose
 
 # Install and update yay
 git clone https://aur.archlinux.org/yay.git
