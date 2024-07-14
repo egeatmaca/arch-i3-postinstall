e0b54a35015ced52b51bf9c6412de14fe4d85e50
egeatmaca <egeatmaca98@gmail.com>
2024-07-14 16:38:37 +0200

    vs code removed, laziyvim added

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,7 @@
 sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Update and install pacman packages
-sudo pacman -Syu --needed --noconfirm alacritty rofi thunar feh picom htop neofetch bluez bluez-utils blueman redshift firefox ufw code neovim git python-pip docker docker-buildx docker-compose
+sudo pacman -Syu --needed --noconfirm alacritty rofi thunar feh picom htop neofetch bluez bluez-utils blueman redshift firefox ufw neovim git python-pip docker docker-buildx docker-compose
 
 # Install and update yay
 git clone https://aur.archlinux.org/yay.git
@@ -14,11 +14,16 @@ cd ..
 sudo rm -r yay
 yay
 
+# Install LazyVim
+mv ~/.config/nvim{,.bak}
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
 # Enable installed services
 sudo systemctl enable --now bluetooth
 sudo ufw enable
 
 # Copy dotfiles
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 sudo cp -a $SCRIPT_DIR/dotfiles/root/. /
 cp -a $SCRIPT_DIR/dotfiles/user/. ~
