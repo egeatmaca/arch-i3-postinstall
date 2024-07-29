3d320f64cffeb4868f2ad76a6a8624ab324e2071
egeatmaca <egeatmaca98@gmail.com>
2024-07-29 04:11:21 +0200

    code refactor and additional packages

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,18 @@
 ### Arch I3 Postinstall ###
 
+# Get script directory
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
 # Update mirrors
 sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist
 
-# Update and install pacman packages
-sudo pacman -Syu --needed --noconfirm alacritty rofi thunar feh picom htop neofetch bluez bluez-utils blueman redshift firefox ufw neovim git python-pip docker docker-buildx docker-compose xclip otf-geist-mono-nerd
+# Update packages
+sudo pacman -Syu
+
+# Install packages in packages.txt using pacman
+while read package; do
+  sudo pacman -S "$package" --needed --noconfirm
+done <$SCRIPT_DIR/packages.txt
 
 # Install and update yay
 git clone https://aur.archlinux.org/yay.git
@@ -24,6 +32,5 @@ sudo systemctl enable --now bluetooth
 sudo ufw enable
 
 # Copy dotfiles
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 sudo cp -a $SCRIPT_DIR/dotfiles/root/. /
 cp -a $SCRIPT_DIR/dotfiles/user/. ~
