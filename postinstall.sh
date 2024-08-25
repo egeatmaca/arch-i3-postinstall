87d3e111d2c0519633cb25d404ac04c7c3c7494f
egeatmaca <egeatmaca98@gmail.com>
2024-08-25 11:54:49 +0200

    apparmor added

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,7 @@
 ### Arch I3 Postinstall ###
 
-# Constants
+# Get script directory
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-PACKAGE_LISTS=$SCRIPT_DIR/package-lists
 
 # Update mirrors
 sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist
@@ -13,7 +12,7 @@ sudo pacman -Syu
 # Install pacman packages
 while read package; do
   sudo pacman -S "$package" --needed --noconfirm
-done <$PACKAGE_LISTS/pacman.txt
+done <$SCRIPT_DIR/package-lists/pacman.txt
 
 # Install and update yay
 git clone https://aur.archlinux.org/yay.git
@@ -26,20 +25,24 @@ yay
 # Install AUR packages
 while read package; do
   yay -S "$package" --needed --noconfirm
-done <$PACKAGE_LISTS/aur.txt
+done <$SCRIPT_DIR/package-lists/aur.txt
 
 # Install LazyVim
 mv ~/.config/nvim{,.bak}
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
-# Enable installed services
-sudo systemctl enable --now bluetooth
-sudo ufw enable
-
 # Copy dotfiles
 sudo cp -a $SCRIPT_DIR/dotfiles/root/. /
 cp -a $SCRIPT_DIR/dotfiles/user/. ~
+
+# Update grub using the new config
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# Enable installed services
+sudo systemctl enable --now apparmor
+sudo systemctl enable --now bluetooth
+sudo ufw enable
 
 # Create user directories
 mkdir ~/Downloads ~/Documents ~/Pictures ~/Music ~/Git
